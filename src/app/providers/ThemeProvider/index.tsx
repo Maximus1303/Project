ca9f8dcdd3829f08205db6c5d26f2d621bf7177d
Maximus1303 <maximus13032@gmail.com>
2024-12-17 11:03:22 +0300

    Fix theme

--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -2,4 +2,7 @@ import ThemeProvider from "./ui/ThemeProvider";
 import {useTheme} from "./lib/useTheme";
 
 
-export { ThemeProvider, useTheme };
+export {
+    ThemeProvider,
+    useTheme,
+};
--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,18 +1,22 @@
-
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
 import {useContext} from "react";
-import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "shared/config/theme/ThemeContext";
 
 interface UseThemeResult {
     toggleTheme: () => void;
-    theme: Theme
+    theme: Theme;
 }
 
-export function useTheme(): UseThemeResult  {
-    const {theme, setTheme} = useContext(ThemeContext)
+export function useTheme(): UseThemeResult {
+    const {theme, setTheme} = useContext(ThemeContext);
+
     const toggleTheme = () => {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
         setTheme(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
     }
-    return { toggleTheme, theme};
+
+    return {
+        theme,
+        toggleTheme
+    }
 }
