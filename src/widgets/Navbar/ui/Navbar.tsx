a65f561486e129daf9e2f4851aeca85a9108697b
Maximus1303 <maximus13032@gmail.com>
2024-12-19 12:09:41 +0300

    add jest tests

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -7,6 +7,7 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => (
+
     <div className={classNames(cls.Navbar, {}, [className])}>
         <div className={cls.links}>
             <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
